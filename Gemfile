eb6e8d20227cb5204aa8fc31c62a2af85f0d384b
Mike Kasper <miker.kasper@gmail.com>
2015-02-27 18:03:34 -0700

    Initial copy of 2nd attemp

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'jquery-rails'
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
+gem 'haml', '~> 4.0.5'
+gem 'simple_form', '~> 3.0.2'
+
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,50 @@
+class RecipesController < ApplicationController
+	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
+
+		def index
+			@recipe = Recipe.all.order("created_at DESC")
+		end
+
+		def show
+		end
+
+		def new
+			@recipe = current_user.recipes.build
+		end
+
+		def create
+			@recipe = current_user.recipes.build(recipe_params)
+			if @recipe.save
+					redirect_to @recipe, notice: "Recipe successfully created"
+			else
+					render 'new'
+			end
+		end
+
+		def edit
+		end
+
+		def update
+				if @recipe.update(recipe_params)
+						redirect_to @recipe
+			else
+					render 'edit'
+			end
+		end
+
+		def destroy
+				@recipe.destroy
+				redirect_to root_path, notice: "Recipe deleted successfully"
+		end
+
+		private
+
+		def recipe_params
+				params.require(:recipe).permit(:title, :description)
+		end
+
+		def find_recipe
+				@recipe = Recipe.find(params[:id])
+		end
+end
