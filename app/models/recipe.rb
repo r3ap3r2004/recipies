5ab527d2d591b80a520b41a54066e2def3239647
Mike Kasper <miker.kasper@gmail.com>
2015-03-02 16:54:40 -0700

    fixes with Chrises assistance

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,20 +1,19 @@
 class Recipe < ActiveRecord::Base
-	belongs_to :user
+  belongs_to :user
 
-	has_many :ingredients
-	has_many :directions
+  has_many :ingredients
+  has_many :directions
 
-	accepts_nested_attributes_for :ingredients
-								   reject_if:proc { |attributes| attributes['name'].blank? },
-								   allows_destroy: true
+  accepts_nested_attributes_for :ingredients
+  reject_if:proc { |attributes| attributes['name'].blank? },
+  allows_destroy: true
 			
-	accepts_nested_attributes_for :directions
-								   reject_if:proc { |attributes| attributes['step'].blank? },
-								   allows_destroy: true	
-								end
+  accepts_nested_attributes_for :directions
+  reject_if:proc { |attributes| attributes['step'].blank? },
+  allows_destroy: true	
 
-	validates :title, :description, :image, :presense: true	
+  validates :title, :description, :image, :presense: true	
 
-		has_attached_file :image, styles: { :medium => "400x400#" }
-		validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { :medium => "400x400#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
